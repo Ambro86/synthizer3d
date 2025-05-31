1b264e04b31a8092f9a3d3170870c1f73d4f704b
ambro86 <ambro86@gmail.com>
2025-05-31 06:53:49 +0200

    Added gitignore and modified ci.yaml.

--- _skbuild/win-amd64-3.13/cmake-build/_deps/pdqsort-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/fork/synthizer3d/synthizer-vendored/deps/deps/third_party/pdqsort
-# Build directory: C:/fork/synthizer3d/_skbuild/win-amd64-3.13/cmake-build/_deps/pdqsort-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

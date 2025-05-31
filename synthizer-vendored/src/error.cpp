bfd56a548aec7a90bc2143c392381667204d57ab
ambro86 <ambro86@gmail.com>
2025-05-31 07:20:29 +0200

    Deleted some folders and edited gitignore.

--- synthizer-vendored/src/error.cpp
+++
@@ -1,9 +0,0 @@
-#include "synthizer/error.hpp"
-
-namespace synthizer {
-
-Error::Error(const std::string &message) : message(message) {}
-
-const std::string &Error::getMessage() const { return this->message; }
-
-} // namespace synthizer
